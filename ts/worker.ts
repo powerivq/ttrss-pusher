cdefa1bea76ba31ae98296efbc827afc3eab98c6
powerivq <poweriv.q@gmail.com>
2020-06-28 18:51:50 -0700

    Change to default message notification

--- ts/worker.ts
+++ ts/worker.ts
@@ -1,10 +1,7 @@
-import {uuidv4} from './utils';
-
 declare const self: ServiceWorkerGlobalScope;
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 interface ServiceWorkerGlobalScope {
-  displayQueue: Array<string>;
   registration: any;
   addEventListener: any;
   clients: any;
@@ -23,72 +20,15 @@ function supportsImageProp(): boolean {
   return !/Mac OS X/.test(navigator.userAgent);
 }
 
-function closeMessage(notification: Notification): void {
-  if (!notification.data) {
-    return;
-  }
-  if (notification.data.jobId != self.displayQueue[0]) {
-    console.error('Unmatched message and head of queue');
-  } else {
-    self.displayQueue = self.displayQueue.slice(1);
-  }
-}
-
-self.displayQueue = [];
-function showMessage(data: Data): Promise<void> {
-  const jobId = uuidv4();
-  self.displayQueue.push(jobId);
-  return Promise.resolve()
-    .then(
-      () =>
-        new Promise<number>((resolve) => {
-          let timerId: number = undefined;
-          const checkPosition = (): boolean => {
-            if (self.displayQueue[0] != jobId) return false;
-            if (timerId !== undefined) clearInterval(timerId);
-            data.jobId = jobId;
-            const title = data.title;
-            const options = {
-              data: data,
-              body: data.excerpt,
-              icon: supportsImageProp() ? undefined : data.image,
-              image: data.image,
-              silent: true,
-              requireInteraction: true,
-            };
-            self.registration.showNotification(title, options);
-            resolve(new Date().getTime());
-            return true;
-          };
-
-          if (checkPosition()) return;
-          timerId = self.setInterval(checkPosition, 1000);
-        })
-    )
-    .then(
-      (startTime) =>
-        new Promise((resolve) => {
-          const timerId = setInterval(() => {
-            if (new Date().getTime() - startTime > 20000) {
-              self.registration.getNotifications().then((notifications) => {
-                for (let i = 0; i < notifications.length; i++) {
-                  if (
-                    notifications[i].data &&
-                    notifications[i].data.jobId == data.jobId
-                  ) {
-                    notifications[i].close();
-                    closeMessage(notifications[i]);
-                  }
-                }
-              });
-            }
-            if (self.displayQueue.length && self.displayQueue[0] == jobId)
-              return;
-            clearInterval(timerId);
-            resolve();
-          }, 500);
-        })
-    );
+function showMessage(data: Data): void {
+  const options = {
+    data: data,
+    body: data.excerpt,
+    icon: supportsImageProp() ? undefined : data.image,
+    image: data.image,
+    silent: true,
+  };
+  self.registration.showNotification(data.title, options);
 }
 
 self.addEventListener('push', function (event) {
@@ -100,7 +40,6 @@ self.addEventListener('push', function (event) {
 
 self.addEventListener('notificationclick', function (event) {
   event.notification.close();
-  closeMessage(event.notification);
 
   const data = event.notification.data;
   const headers = new Headers({
@@ -121,6 +60,4 @@ self.addEventListener('notificationclick', function (event) {
   );
 });
 
-self.addEventListener('notificationclose', function (event) {
-  closeMessage(event.notification);
-});
+export {};
